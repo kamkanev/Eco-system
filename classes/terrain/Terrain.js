3ffeeda4258092d6787fd13ebffea29b3e94583a
kamkanev <kamkanev@gmail.com>
2021-03-30 18:35:34 +0300

    cool terrain generation + zoom, move and other modes added

--- classes/terrain/Terrain.js
+++ classes/terrain/Terrain.js
@@ -7,7 +7,7 @@ const TERRAIN_TYPES = {
 };
 
 class Terrain {
-  constructor() {
+  constructor(isInverted = false) {
 
     this.noiseScale = {
       x: (Math.random() < .90) ? Number((Math.random()/10).toFixed(3)) : Number((Math.random()/5).toFixed(3)),
@@ -16,17 +16,22 @@ class Terrain {
     }
 
     this.map = [];
-    this.zoom = 15;
+    this.zoom = 30;
+    this.isInBuildMode = false;
+    this.isInverted = isInverted;
     this.deltaPoint = new Point();
+
+    this.init();
 
   }
 
   init(){
 
-    // this.imageData = c2d.createImageData(canvas.width, canvas.height);
-    // var data = this.imageData.data;
-    // var minN = 1, maxTh = 0;
+    this.__generateNewTerrain();
+  }
 
+  __generateNewTerrain(){
+    this.map = [];
     for (let x = 0; x < canvas.width; x++) {
       this.map[x] = [];
       for (var y = 0; y < canvas.height; y++) {
@@ -43,8 +48,6 @@ class Terrain {
         this.__initMap(x, y, noiseVal);
       }
     }
-
-            // console.log(data.length);
   }
 
   __initMap(x, y, noise){
@@ -55,21 +58,35 @@ class Terrain {
 
   getRandomType(noise){
 
-    if(noise < 0.30){
-      return TERRAIN_TYPES.WATER;
-    }else if(noise < 0.40){
-      return TERRAIN_TYPES.SAND
-    }else if(noise < 0.60){
-      return TERRAIN_TYPES.GRASS;
-    }else if(noise < 0.80){
-      return TERRAIN_TYPES.ROCKS;
-    }else if(noise < 0.90){
-      return TERRAIN_TYPES.SNOW;
+    if(!this.isInverted){
+      if(noise < 0.30){
+        return TERRAIN_TYPES.WATER;
+      }else if(noise < 0.40){
+        return TERRAIN_TYPES.SAND
+      }else if(noise < 0.60){
+        return TERRAIN_TYPES.GRASS;
+      }else if(noise < 0.80){
+        return TERRAIN_TYPES.ROCKS;
+      }else if(noise < 0.90){
+        return TERRAIN_TYPES.SNOW;
+      }
+    }else{
+      if(noise < 0.10){
+        return TERRAIN_TYPES.SNOW;
+      }else if(noise < 0.20){
+        return TERRAIN_TYPES.ROCKS
+      }else if(noise < 0.40){
+        return TERRAIN_TYPES.GRASS;
+      }else if(noise < 0.50){
+        return TERRAIN_TYPES.SAND;
+      }else{
+        return TERRAIN_TYPES.WATER;
+      }
     }
 
   }
 
-  move(x, y){
+  move(x = 0, y = 0){
     if(Math.round(this.map.length/this.zoom) + x < canvas.width){
       if(Math.round(this.map[0].length/this.zoom) + y < canvas.height){
         if(x >= 0 && y >= 0){
@@ -79,27 +96,61 @@ class Terrain {
     }
   }
 
+  changeZoom(z = 30){
+    if(z >= 1 && z <= 200){
+      this.zoom = z;
+    }else if(z < 1){
+      this.zoom = 1;
+    }else if(z > 200){
+      this.zoom = 200;
+    }
+  }
+
+  setBuildMode(b = true){
+    this.isInBuildMode = b;
+  }
+
+  changeBuildMode(){
+    this.isInBuildMode = !this.isInBuildMode;
+  }
+
   draw(){
 
+    var size = this.zoom;
+
+    if(this.isInBuildMode){
+      size--;
+    }
+
     for (var x = this.deltaPoint.x; x < Math.round(this.map.length/this.zoom)+this.deltaPoint.x; x++) {
-      for (var y = this.deltaPoint.y; y < Math.round(this.map[x].length/this.zoom)+this.deltaPoint.y; y++) {
-        var c = this.map[x][y];
-        // console.log(c);
-        if(c == TERRAIN_TYPES.WATER){
-          context.fillStyle = "#0000ff";
-        }else if(c == TERRAIN_TYPES.SAND){
-          context.fillStyle = "#fff000";
-        }else if(c == TERRAIN_TYPES.GRASS){
-          context.fillStyle = "#00ff00";
-        }else if(c == TERRAIN_TYPES.ROCKS){
-          context.fillStyle = "#787878";
-        }else if(c == TERRAIN_TYPES.SNOW){
-          context.fillStyle = "#ffffff";
+
+      if(this.map[x] != undefined){
+
+        for (var y = this.deltaPoint.y; y < Math.round(this.map[x].length/this.zoom)+this.deltaPoint.y; y++) {
+
+          if(this.map[x][y] != undefined){
+
+            var c = this.map[x][y];
+            // console.log(c);
+            if(c == TERRAIN_TYPES.WATER){
+              context.fillStyle = "#0000ff";
+            }else if(c == TERRAIN_TYPES.SAND){
+              context.fillStyle = "#fff000";
+            }else if(c == TERRAIN_TYPES.GRASS){
+              context.fillStyle = "#00ff00";
+            }else if(c == TERRAIN_TYPES.ROCKS){
+              context.fillStyle = "#787878";
+            }else if(c == TERRAIN_TYPES.SNOW){
+              context.fillStyle = "#ffffff";
+            }
+
+            context.fillRect((x - this.deltaPoint.x)*this.zoom, (y - this.deltaPoint.y)*this.zoom, size, size);
+
+          }
+
         }
-
-        context.fillRect((x - this.deltaPoint.x)*this.zoom, (y - this.deltaPoint.y)*this.zoom, this.zoom, this.zoom);
-
       }
+
     }
 
   }
--- game.js
+++ game.js
@@ -9,35 +9,19 @@ var arr = [];
 
 var t = new Terrain();
 
-t.init();
-
-// for (let y = 0; y < canvas.height; y++) {
-//   arr[y] = [];
-//   for (var x = 0; x < canvas.width; x++) {
-//     let noiseVal = PerlinNoise.noise(x*noiseScale, y*noiseScale, .8);
-//     // console.log(`noise: ${noiseVal*80}, all: ${noiseVal*255}`);
-//     arr[y][x] = {
-//       x: noiseVal*80,
-//       y : noiseVal*80,
-//       c : Math.round(noiseVal * 255)
-//     }
-//   }
-// }
-
-var myX = 0, myY = 0;
+var cursor = new Point();
 
 function update() {
-    myX = myX+(mouseX-myX)/10;
-    myY = myY+(mouseY-myY)/10;
+    cursor.x = cursor.x+(mouseX-cursor.x);
+    cursor.y = cursor.y+(mouseY-cursor.y);
 }
 
 function draw() {
     // This is how you draw a rectangle
-
     t.draw();
 
-    context.fillColor = "blue";
-    context.fillRect(myX, myY, 30, 30);
+    context.fillStyle = "pink";
+    context.fillRect(cursor.x, cursor.y, 30, 30);
 
 
 };
@@ -45,6 +29,40 @@ function draw() {
 function keyup(key) {
     // Show the pressed keycode in the console
     console.log("Pressed", key);
+
+    if(key == 66){//b
+      t.changeBuildMode();
+    }
+};
+
+function keydown(key) {
+    // Show the pressed keycode in the console
+    console.log("PressedDown", key);
+
+    if(isKeyPressed[90]){//z
+      t.changeZoom(t.zoom + 5);
+    }
+
+    if(isKeyPressed[88]){//x
+      t.changeZoom(t.zoom - 5);
+    }
+
+
+    if(isKeyPressed[37]){//left
+      t.move(t.deltaPoint.x-1, t.deltaPoint.y);
+    }
+
+    if(isKeyPressed[39]){//right
+      t.move(t.deltaPoint.x+1, t.deltaPoint.y);
+    }
+
+    if(isKeyPressed[38]){//up
+      t.move(t.deltaPoint.x, t.deltaPoint.y-1);
+    }
+
+    if(isKeyPressed[40]){//down
+      t.move(t.deltaPoint.x, t.deltaPoint.y+1);
+    }
 };
 
 function mouseup() {
